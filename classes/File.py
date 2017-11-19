4ec25c1b049787d394ceb3c031c9f333dd4d3bc0
Reece Benson <me@reecebenson.me>
2017-11-19 23:05:28 +0000

    File method additions. Started implementation on loading previous application session but discarded changes.

--- classes/File.py
+++ classes/File.py
@@ -31,12 +31,12 @@ class File():
             # Check our Season exists
             if(season in data):
                 # Check if our setting exists
-                if(name in data['settings']):
+                if(name in data[season]['settings']):
                     # Update setting
-                    data['settings'][name] = value
+                    data[season]['settings'][name] = value
                 else:
                     # Create setting
-                    data['settings'].update({ name: value })
+                    data[season]['settings'].update({ name: value })
 
                 # Seek back to SOF and write back our data
                 f.seek(0)
@@ -44,6 +44,46 @@ class File():
                 f.truncate()
             else:
                 return False
+        return True
+    
+    def get_global_setting(name):
+        with open('data/seasons.json', 'r+') as f:
+            data = json.load(f)
+
+            if("settings" in data):
+                if(name in data["settings"]):
+                    return data["settings"][name]
+            
+            return None
+
+    def get_seasons():
+        seasonIds = [ ]
+        with open('data/seasons.json', 'r+') as f:
+            data = json.load(f)
+
+            for season in data:
+                if(season == "settings"):
+                    continue
+                seasonIds.append(season)
+            
+        return seasonIds
+
+    def update_global_settings(name, value):
+        with open('data/seasons.json', 'r+') as f:
+            data = json.load(f)
+
+            # Check if our setting exists
+            if(name in data['settings']):
+                # Update setting
+                data['settings'][name] = value
+            else:
+                # Create setting
+                data['settings'].update({ name: value })
+
+            # Seek back to SOF and write back our data
+            f.seek(0)
+            f.write(json.dumps(data, indent=4, cls=SmallJSON))
+            f.truncate()
         return True
 
     def update_file_saving(season, tournament, value):
